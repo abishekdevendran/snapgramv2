1a7dad557b2df34868f62c6d6281ab697c4b0ec0
Abishek Devendran <abishekdevendran@gmail.com>
2024-11-10 17:23:13 +0530

    feat: Update Redis client import to use Cloudflare version and remove custom agent

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,10 +1,9 @@
-import { Redis } from '@upstash/redis';
+import { Redis } from '@upstash/redis/cloudflare';
 import { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } from '$env/static/private';
 
 const redisClient = new Redis({
 	url: UPSTASH_REDIS_REST_URL,
 	token: UPSTASH_REDIS_REST_TOKEN,
-	agent: 'SnapGram',
 	automaticDeserialization: false,
 	retry: {
 		retries: 5
