b82f1b1ada97568313fc4194ef0bb5b1df1319e9
Abishek Devendran <abishekdevendran@gmail.com>
2024-11-10 20:55:14 +0530

    Revert "feat: Integrate Sentry for error tracking and monitoring; add Sentry …"

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,6 @@
-import { sequence } from '@sveltejs/kit/hooks';
-import * as Sentry from '@sentry/sveltekit';
 import type { Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/session';
 import { encodeBase32LowerCase } from '@oslojs/encoding';
-
-Sentry.init({
-	dsn: 'https://c407ca5909c30175b1988f24d15081a7@o4508274259263488.ingest.de.sentry.io/4508274273026128',
-	tracesSampleRate: 1
-});
 
 const DAY_IN_MS = 1000 * 60 * 60 * 24;
 
@@ -38,5 +31,4 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
-export const handle: Handle = sequence(Sentry.sentryHandle(), handleAuth);
-export const handleError = Sentry.handleErrorWithSentry();
+export const handle: Handle = handleAuth;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'abishek-3f',
-				project: 'snapgram'
-			}
-		}),
-		sveltekit()
-	],
+	plugins: [sveltekit()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
